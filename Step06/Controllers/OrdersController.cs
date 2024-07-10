a952398f32bdd2d8ea3002e68b814f8cf428bf06
ukaracivio <umit.karacivi@outlook.com>
2024-07-10 20:39:28 +0300

    Some changes

---
+++ Step06/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+        //private readonly IOrdersService _ordersService;
+
+
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            //string userId = "";
+            //string userRole = "";
+
+            //var orders= await _ordersService.GetAll();//
+
+
+
+            return View();
+        }
+    }
+}
--- Step06/Data/Cart/ShoppingCart.cs
+++ Step06/Data/Cart/ShoppingCart.cs
@@ -14,17 +14,17 @@ namespace eTickets.Data.Cart
 
         public ShoppingCart(AppDbContext context)
         {
-            _context = context;                
+            _context = context;
         }
 
         public static ShoppingCart GetShoppingCart(IServiceProvider services)
         {
             // Bu tür işlemlerde bellekte kullanımiçin Session dediğimiz bir yapı kullanılır. Onu tanımlayalım/yerleştirelim.
-            ISession session= services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
 
             var context = services.GetService<AppDbContext>();
 
-            string cartId= session.GetString("CartId") ?? Guid.NewGuid().ToString(); // CardId isimli Sessiondan ben herhangi bir bilgi alamıyorsam bunu yeni oluştur.
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString(); // CardId isimli Sessiondan ben herhangi bir bilgi alamıyorsam bunu yeni oluştur.
 
             session.SetString("CartId", cartId); // Session a CartId yi yerleştir.
 
@@ -35,6 +35,7 @@ namespace eTickets.Data.Cart
         // 57
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
+            // ShoppingCart tablosundaki kayıtlar
             // ?? -- isnull
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
                 .Where(c => c.ShoppingCartId == ShoppingCartId)
@@ -45,6 +46,7 @@ namespace eTickets.Data.Cart
 
         public double GetShoppingCartTotal()
         {
+            // ShoppingCart tablosundaki Toplam ücreti verecek.
             var total = _context.ShoppingCartItems
                 .Where(c => c.ShoppingCartId == ShoppingCartId)
                 .Select(c => c.Movie.Price * c.Amount)
@@ -53,6 +55,54 @@ namespace eTickets.Data.Cart
             return total;
         }
 
+        public void AddItemToCart(Movie movie)
+        {
+            // ShoppingCart a bir item ekleme
 
+            // Öncelikle ilgili Movie daha önceden ShoppingCart a eklenmiş mi??
+
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                // Yani film daha önceden sepete atılmamışsa
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++; // daha önceden varsa bir arttır.
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem !=null)
+            {
+                // Vardır...
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                }
+                else
+                {
+                    // eğer 1 tane varda VT den kaldır.
+                    _context.ShoppingCartItems.Remove(shoppingCartItem) ;
+                }
+            }
+            _context.SaveChanges();
+
+        }
+
+        // ??
     }
 }
